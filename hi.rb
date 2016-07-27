3850da822459817af048c47ac04d7399aae8fb46
Naga Jothi <gknagajothi@gmail.com>
2016-07-27 12:22:06 +0530

    ndj

--- hi.rb
+++ hi.rb
@@ -3,4 +3,29 @@ puts "new to ruby"
 puts"learning basics"
 x=1
 y=5
-puts "sum of lessons in ruby #{x+y}"
+puts "sum of lessons in ruby #{x+y}" 
+puts "."*10
+puts "after this im going to study"+" ruby"+" on rails"
+print "think"
+print "think"
+puts "think"
+puts "think"
+puts "this is the given. 
+changes in ruby"
+puts """this is the given
+change in ruby"""
+puts"this is the given \n change in ruby using"
+puts"this is the given \n\t change in ruby"
+a=9
+b=3.7
+c="33"
+d=99.09
+e="str"
+puts"""#{a.class}
+#{b.class}
+#{c.class}
+#d.class
+#{d.class}
+#{e.class}"""
+
+
