e818c8a66b27e48ab377751d633df1996865fdb1
Naga Jothi <gknagajothi@gmail.com>
2016-08-10 14:27:14 +0530

    Implement basic login

--- workspace/sample_app_1/config/routes.rb
+++ workspace/sample_app_1/config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  get 'sessions/new'
+
   root 'stat_page#home'
 
   get '/help', to: 'stat_page#help'
@@ -10,6 +12,10 @@ Rails.application.routes.draw do
 
   get  '/signup',  to: 'users#new'
   post '/signup',  to: 'users#create'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
+
 
   get 'static_page/home'
   get'static_page/help'
