4bd8658c18c3152a9c2a2c9ee3d97bf3a72fd9a2
Naga Jothi <gknagajothi@gmail.com>
2016-08-16 12:21:29 +0530

    Implement advanced login

--- workspace/sample_app_1/test/models/user_test.rb
+++ workspace/sample_app_1/test/models/user_test.rb
@@ -63,7 +63,9 @@ test "name should not be too long" do
     @user.password = @user.password_confirmation = "a" * 5
     assert_not @user.valid?
   end
-
-
+ 
+  test "authenticated? should return false for a user with nil digest" do
+    assert_not @user.authenticated?('')
+  end
 
 end
