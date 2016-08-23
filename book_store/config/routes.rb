1b6b86fc9aa07732fb363fd15df04fd663badc9c
Naga Jothi <gknagajothi@gmail.com>
2016-08-23 16:05:13 +0530

    layout and routes

--- book_store/config/routes.rb
+++ book_store/config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+
   root 'basic_page#home'
 
   get '/login', to: 'basic_page#login'
+
+  get '/signup', to: 'users#new'
 
 end
---
+++ book_store/test/integration/site_layout_test.rb
@@ -0,0 +1,10 @@
+require 'test_helper'
+
+class SiteLayoutTest < ActionDispatch::IntegrationTest
+  test "layout links" do
+    get root_path
+    assert_template 'basic_page/home'
+    assert_select "a[href=?]", root_path, count: 2
+    assert_select "a[href=?]", login_path
+  end
+end
