df431cc58f6b267d85422416a3bb0488a98687ee
Naga Jothi <gknagajothi@gmail.com>
2016-08-24 11:42:18 +0530

    signup page

--- book_store/config/routes.rb
+++ book_store/config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
 
   root 'basic_page#home'
-
   get '/login', to: 'basic_page#login'
-
   get '/signup', to: 'users#new'
+  post '/signup',  to: 'users#create'
+  resources :users
 
 end
