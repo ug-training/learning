8b9bc3ea9a6f0722e36e4f47eddbd36d5ec6eb4b
Naga Jothi <gknagajothi@gmail.com>
2016-08-24 16:17:16 +0530

    login check

--- book_store/config/routes.rb
+++ book_store/config/routes.rb
@@ -3,13 +3,11 @@ Rails.application.routes.draw do
   get 'sessions/new'
 
   root 'basic_page#home'
-#  get '/login', to: 'basic_page#login'
   get '/signup', to: 'users#new'
   post '/signup',  to: 'users#create'
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
-  resources :users
   resources :users
 
 end
